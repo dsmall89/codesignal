7fda3bf4d296c49409229909bfacc9a847fa2688
Donte Small <small.donte@yahoo.com>
2020-12-20 23:45:14 -0500

    Added to isListPalindrome

--- isCryptSolution.py
+++ isCryptSolution.py
@@ -1,15 +1,18 @@
-# def isCryptSolution(crypt, solution):
+'''
+A cryptarithm is a mathematical puzzle for which the goal is to find the correspondence between letters and digits, such that the given arithmetic equation consisting of letters holds true when the letters are converted to digits.
 
-def extractChar(crypt,solution):
-    listtify_word= list(map(list,crypt))
-    print(listtify_word)
-    for i in crypt:
+* You have an array of strings crypt, the cryptarithm, 
+  and an an array containing the mapping of letters and digits, solution. 
 
-        for j in  i:
+* The array crypt will contain three non-empty strings that follow the structure: [word1, word2, word3], 
+   which should be interpreted as the word1 + word2 = word3 cryptarithm.
 
-            for key,value in solution:
-                if j == key:
-                    print(value)
+* If crypt, when it is decoded by replacing all of the letters in the cryptarithm with digits using the mapping in solution,
+  becomes a valid arithmetic equation containing no numbers with leading zeroes, 
+  the answer is true. If it does not become a valid arithmetic solution, the answer is false.
+
+ -> Note that number 0 doesn't contain leading zeroes (while for example 00 or 0123 do).
+'''
 
 crypt = ["SEND", "MORE", "MONEY"]
 solution = [['O', '0'],
---
+++ isListPalindrome.py
@@ -0,0 +1,24 @@
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+
+'''
+Algorithm:  O(n) time, 0(n) space
+1. For every element in the LL, fill it into a Array 
+2. After the array is filled, reverse one the array
+3. Compare the reversed array to original array for equality
+Note: Cost about n space and we have to reverse one array
+'''
+def isListPalindrome(head) :
+    if not head:
+            return True
+        
+    arr = []
+    while head:
+        arr.append(head.value)
+        head = head.next
+    
+    return arr == arr[::-1]
